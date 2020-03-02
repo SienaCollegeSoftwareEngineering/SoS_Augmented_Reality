066d70003377497865d3d4f357d46f3032156f09
Abdul Samad <31701831+a01sama@users.noreply.github.com>
2020-03-02 17:30:20 -0500

    added 348

---
+++ Tryout/Assets/Scripts/Link.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Link : MonoBehaviour
+{
+    public void OpenCS()
+    {
+        Application.OpenURL("https://www.siena.edu/departments/computer-science/");
+    }
+
+    public void OpenSoftDev()
+    {
+        Application.OpenURL("https://www.siena.edu/programs/software-development/");
+    }
+
+    public void OpenInfoSys()
+    {
+        Application.OpenURL("https://www.siena.edu/programs/information-systems/");
+    }
+}
