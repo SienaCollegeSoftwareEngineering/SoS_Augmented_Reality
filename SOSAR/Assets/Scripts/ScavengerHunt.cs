97b133575708dc4d995fa332fc31b30a1990d22a
Jaclyn Wirth <ja01wirt@siena.edu>
2023-11-17 16:25:04 -0500

    Finished basic game logic

--- SOSAR/Assets/Scripts/ScavengerHunt.cs
+++ SOSAR/Assets/Scripts/ScavengerHunt.cs
@@ -17,66 +17,85 @@ public class ScavengerHunt : MonoBehaviour
 
     public static string global_PlaqueInfo;
 
-    public List<string> plaqueNums = new List<string>{"RB316", "RB319"};
+    public List<string> plaqueNums = new List<string> { "RB316", "RB319" };
 
-    public List<string> plaqueHints = new List<string>{"Lim", "Cotler"};
-    public int current_Plaque = 0;   
-    
-    
+    public List<string> plaqueHints = new List<string> { "Lim", "Cotler" };
+    public int current_Plaque = 0;
 
-public void GetCurrentPlaque(string plaqueInfo) {
-    global_PlaqueInfo = plaqueInfo;
-    Debug.Log(global_PlaqueInfo);
-    checkPlaque();
-        
-}
 
-public void checkPlaque(){
-    if(global_PlaqueInfo == plaqueNums[current_Plaque]){
-        Debug.Log("Correct Plaque");
-        newPlaque();
+
+    public void GetCurrentPlaque(string plaqueInfo)
+    {
+        global_PlaqueInfo = plaqueInfo;
+        Debug.Log(global_PlaqueInfo);
+        checkPlaque();
+
     }
-    else{
-        Debug.Log("Incorrect Plaque");
+
+    public void checkPlaque()
+    {
+        if (plaqueHints.Count != 0)
+        {
+            if (string.Equals(global_PlaqueInfo, plaqueNums[current_Plaque]))
+            {
+                Debug.Log("Correct Plaque");
+                plaqueNums.RemoveAt(current_Plaque);
+                plaqueHints.RemoveAt(current_Plaque);
+                if (plaqueHints.Count == 0)
+                {
+                    Debug.Log("Won Game");
+                }
+                else
+                {
+                    newPlaque();
+                }
+            }
+            else
+            {
+                Debug.Log("Incorrect Plaque");
+            }
+        }
     }
-}
 
-public void newPlaque(){
-    current_Plaque = Random.Range(-1, 2);
-    Debug.Log(current_Plaque);
-}
+    public void newPlaque()
+    {
+        current_Plaque = Random.Range(-1, plaqueNums.Count);
+        Debug.Log(current_Plaque);
+    }
 
-public void displayCurrentHint(){
-    string hint;
-    hint = plaqueNums[current_Plaque];
-    Debug.Log(hint);
-    clueText = GameObject.Find("ClueText").GetComponent<TMP_Text>();
-    clueText.enabled = true;
-    clueText.text = plaqueHints[current_Plaque];
+    public void displayCurrentHint()
+    {
+        string hint;
+        hint = plaqueNums[current_Plaque];
+        Debug.Log(hint);
+        clueText = GameObject.Find("ClueText").GetComponent<TMP_Text>();
+        clueText.enabled = true;
+        clueText.text = plaqueHints[current_Plaque];
 
-    XButton = GameObject.Find("XButton").GetComponent<Button>();
-    XButton.enabled = true;
+        XButton = GameObject.Find("XButton").GetComponent<Button>();
+        XButton.enabled = true;
 
-    img = GameObject.Find("XButton").GetComponent<Image>();
-    img.enabled = true;
+        img = GameObject.Find("XButton").GetComponent<Image>();
+        img.enabled = true;
 
-    pnl = GameObject.Find("ClueBackground").GetComponent<Image>();
-    pnl.enabled = true;
-}
+        pnl = GameObject.Find("ClueBackground").GetComponent<Image>();
+        pnl.enabled = true;
+    }
 
-public void hideCurrentHint(){
-    clueText = GameObject.Find("ClueText").GetComponent<TMP_Text>();
-    clueText.enabled = false;
+    public void hideCurrentHint()
+    {
+        clueText = GameObject.Find("ClueText").GetComponent<TMP_Text>();
+        clueText.enabled = false;
 
-    XButton = GameObject.Find("XButton").GetComponent<Button>();
-    XButton.enabled = false;
+        XButton = GameObject.Find("XButton").GetComponent<Button>();
+        XButton.enabled = false;
 
-    img = GameObject.Find("XButton").GetComponent<Image>();
-    img.enabled = false;
+        img = GameObject.Find("XButton").GetComponent<Image>();
+        img.enabled = false;
 
-    pnl = GameObject.Find("ClueBackground").GetComponent<Image>();
-    pnl.enabled = false;
-}
+        pnl = GameObject.Find("ClueBackground").GetComponent<Image>();
+        pnl.enabled = false;
+    }
 
 
 }
