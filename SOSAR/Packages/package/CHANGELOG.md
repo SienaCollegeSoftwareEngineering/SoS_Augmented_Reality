264eca3045de9c21e770f989c266fb890ae4aefc
Jaclyn Wirth <ja01wirt@siena.edu>
2023-11-17 16:29:32 -0500

    Updated Ar Objects

---
+++ SOSAR/Packages/package/CHANGELOG.md
@@ -0,0 +1,3 @@
+# Changelog
+All notable changes to the Vuforia Engine package will be documented in this file.
+To view detailed release notes, go to: https://library.vuforia.com/articles/Release_Notes.html
---
+++ SOSAR/Packages/package/Vuforia/Editor/Scripts/OpenSourceInitializer.cs
@@ -0,0 +1,163 @@
+/*===============================================================================
+Copyright (c) 2017-2019 PTC Inc. All Rights Reserved.
+
+Confidential and Proprietary - Protected under copyright and other laws.
+Vuforia is a trademark of PTC Inc., registered in the United States and other 
+countries.
+===============================================================================*/
+#if UNITY_2020_1_OR_NEWER
+using UnityEngine.XR;
+#endif
+#if UNITY_XR_MANAGEMENT
+using UnityEditor.XR.Management;
+using UnityEngine.XR.Management;
+#endif
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using Vuforia;
+using Vuforia.EditorClasses;
+using Vuforia.UnityRuntimeCompiled;
+
+/// <summary>
+/// Creates connection between open source files and the Vuforia library.
+/// Do not modify.
+/// </summary>
+[InitializeOnLoad]
+public static class OpenSourceInitializer
+{
+    static IUnityEditorFacade sFacade;
+
+    static OpenSourceInitializer()
+    {
+        GameObjectFactory.SetDefaultBehaviourTypeConfiguration(new DefaultBehaviourAttacher());
+        ReplacePlaceHolders();
+
+        InitializeFacade();
+        ARFoundationInitializer.InitializeFacade();
+    }
+
+    static void ReplacePlaceHolders()
+    {
+        var observerPlaceholders = Object.FindObjectsOfType<DefaultObserverBehaviourPlaceholder>().ToList();
+        var initErrorsPlaceholders = Object.FindObjectsOfType<DefaultInitializationErrorHandlerPlaceHolder>().ToList();
+
+        observerPlaceholders.ForEach(ReplaceObserverPlaceHolder);
+        initErrorsPlaceholders.ForEach(ReplaceInitErrorPlaceHolder);
+    }
+    
+    static void ReplaceObserverPlaceHolder(DefaultObserverBehaviourPlaceholder placeHolder)
+    {
+        var go = placeHolder.gameObject;
+        var doeh = go.AddComponent<DefaultObserverEventHandler>();
+        SetDefaultObserverHandlerSettings(doeh);
+
+        Object.DestroyImmediate(placeHolder);
+    }
+
+    static void ReplaceInitErrorPlaceHolder(DefaultInitializationErrorHandlerPlaceHolder placeHolder)
+    {
+        var go = placeHolder.gameObject;
+        go.AddComponent<DefaultInitializationErrorHandler>();
+
+        Object.DestroyImmediate(placeHolder);
+    }
+
+    class DefaultBehaviourAttacher : IDefaultBehaviourAttacher
+    {
+        public void AddDefaultObserverEventHandler(GameObject go)
+        {
+            var dteh = go.AddComponent<DefaultObserverEventHandler>();
+            SetDefaultObserverHandlerSettings(dteh);
+        }
+
+        public void AddDefaultAreaTargetEventHandler(GameObject go)
+        {
+            var eventHandler = go.AddComponent<DefaultAreaTargetEventHandler>();
+            SetDefaultObserverHandlerSettings(eventHandler);
+        }
+        
+        public void AddDefaultInitializationErrorHandler(GameObject go)
+        {
+            go.AddComponent<DefaultInitializationErrorHandler>();
+        }
+    }
+
+    static void SetDefaultObserverHandlerSettings(DefaultObserverEventHandler doeh)
+    {
+        if (doeh.gameObject.GetComponent<AnchorBehaviour>() != null)
+        {
+            // render anchors in LIMITED mode
+            doeh.StatusFilter = DefaultObserverEventHandler.TrackingStatusFilter.Tracked_ExtendedTracked_Limited;
+        }
+        else
+        {
+            // the default for all other targets is not to consider LIMITED poses
+            doeh.StatusFilter = DefaultObserverEventHandler.TrackingStatusFilter.Tracked_ExtendedTracked;
+        }
+    }
+    
+    static void InitializeFacade()
+    {
+        if (sFacade != null) return;
+
+        sFacade = new OpenSourceUnityEditorFacade();
+        UnityEditorFacade.Instance = sFacade;
+    }
+
+    class OpenSourceUnityEditorFacade : IUnityEditorFacade
+    {
+        public bool IsTargetingHoloLens()
+        {
+            if (EditorUserBuildSettings.activeBuildTarget != BuildTarget.WSAPlayer)
+                return false;
+#if UNITY_2020_1_OR_NEWER
+            const string UNITY_WINDOWSMR_IDENTIFIER = "Windows Mixed Reality";
+            return XRSettings.supportedDevices.Any(xrDevice => xrDevice.Contains(UNITY_WINDOWSMR_IDENTIFIER));
+#else
+            const string UNITY_WINDOWSMR_IDENTIFIER = "WindowsMR";
+            if (!PlayerSettings.GetVirtualRealitySupported(BuildTargetGroup.WSA)) return false;
+            foreach (var vrSdkName in PlayerSettings.GetVirtualRealitySDKs(BuildTargetGroup.WSA))
+            {
+                if (vrSdkName.Equals(UNITY_WINDOWSMR_IDENTIFIER))
+                {
+                    return true;
+                }
+            }
+            return false;
+#endif
+        }
+
+        public bool IsMagicLeapEnabled()
+        {
+            return PlatformIdentifier.IsMagicLeapXREnabled();
+        }
+    }
+
+    class PlatformIdentifier
+    {
+        const string MAGIC_LEAP_LOADER_ID = "MagicLeapLoader";
+
+        public static bool IsMagicLeapXREnabled()
+        {
+            var isMLEnabled = false;
+#if UNITY_XR_MANAGEMENT
+            EditorBuildSettings.TryGetConfigObject(XRGeneralSettings.k_SettingsKey,out XRGeneralSettingsPerBuildTarget androidBuildSetting);
+        
+            if (androidBuildSetting == null)
+                return false;
+        
+            var androidSettings = androidBuildSetting.SettingsForBuildTarget(BuildTargetGroup.Android);
+            if (androidSettings != null && androidSettings.Manager != null && androidSettings.Manager.activeLoaders != null)
+            {
+                isMLEnabled = androidSettings.Manager.activeLoaders.Any(e =>
+                {
+                    var fullName = e.GetType().FullName;
+                    return fullName != null && fullName.Contains(MAGIC_LEAP_LOADER_ID);
+                });
+            }
+#endif
+            return isMLEnabled;
+        }
+    }
+}
