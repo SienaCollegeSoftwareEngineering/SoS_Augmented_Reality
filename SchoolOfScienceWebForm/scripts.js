543b3e50ead26ba0b2900d5b25b36dc6b2a7269b
Chris Sorros <72184260+chrissorros@users.noreply.github.com>
2023-11-29 19:25:20 -0500

    Made css and js changes to the webform

--- SchoolOfScienceWebForm/scripts.js
+++ SchoolOfScienceWebForm/scripts.js
@@ -6,40 +6,34 @@ $("#submit").click(function(){
     var name = $("#wf_name").val();
     var hobbies = $("#wf_hobbies").val();
     var classes = $("#wf_classes").val();
-    var attest = false;
-    switch($("#wf_attestation").val()){
-        case "on":
-            attest = true;
-            break;
-        case "off":
-            attest = false;
-            break;
+    var attest = $("#wf_attestation").is(":checked");
+    //only exicute if the user has agreed to the attestation
+    if(attest){
+        //create a JSON
+        var data = {
+            "wf_email": email,
+            "wf_office_location": officeLocation,
+            "wf_office_phone": officePhone,
+            "wf_name": name,
+            "wf_hobbies": hobbies,
+            "wf_classes": classes,
+            "wf_attestation": attest,
+        };
+
+        console.log(data);
+
+        //ajax call to send the data to the server
+        //uncomment when ready to send data to server
+        // $.ajax({
+        //     type: "POST",
+        //     url: "",
+        //     data: data,
+        //     success: function(response){
+        //         console.log(response);
+        //     },
+        //     error: function(response){
+        //         console.log(response);
+        //     }
+        // });
     }
-    console.log(attest);
-
-    //create a JSON
-    var data = {
-        "wf_email": email,
-        "wf_office_location": officeLocation,
-        "wf_office_phone": officePhone,
-        "wf_name": name,
-        "wf_hobbies": hobbies,
-        "wf_classes": classes,
-        "wf_attestation": attest,
-    };
-
-    //ajax call to send the data to the server
-    //uncomment when ready to send data to server
-    // $.ajax({
-    //     type: "POST",
-    //     url: "",
-    //     data: data,
-    //     success: function(response){
-    //         console.log(response);
-    //     },
-    //     error: function(response){
-    //         console.log(response);
-    //     }
-    // });
-
 });
