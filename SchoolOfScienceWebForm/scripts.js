af458758c94df0af688f4b38028c2f32dd9bf276
Chris Sorros <72184260+chrissorros@users.noreply.github.com>
2024-01-22 17:18:17 -0500

    Made more updates to the webform.

--- SchoolOfScienceWebForm/scripts.js
+++ SchoolOfScienceWebForm/scripts.js
@@ -1,27 +1,22 @@
 $("#submit").click(function(){
     //get all the values of the form
     var email = $("#wf_email").val();
-    var officeLocation = $("#wf_office_location").val();
-    var officePhone = $("#wf_office_phone").val();
-    var name = $("#wf_name").val();
-    var hobbies = $("#wf_hobbies").val();
-    var classes = $("#wf_classes").val();
+    var fname = $("#wf_fname").val();
+    var lname = $("#wf_lname").val();
+    var about = $("#wf_about").val();
     var attest = $("#wf_attestation").is(":checked");
     //only exicute if the user has agreed to the attestation
     if(attest){
         //create a JSON
         var data = {
             "wf_email": email,
-            "wf_office_location": officeLocation,
-            "wf_office_phone": officePhone,
-            "wf_name": name,
-            "wf_hobbies": hobbies,
-            "wf_classes": classes,
-            "wf_attestation": attest,
+            "wf_fname": fname,
+            "wf_lname": lname,
+            "wf_about": about,
+            "wf_attestation": attest
         };
 
         console.log(data);
-
         //ajax call to send the data to the server
         //uncomment when ready to send data to server
         // $.ajax({
