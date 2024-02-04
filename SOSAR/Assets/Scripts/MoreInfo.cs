347681544ab07b195782f83ef56d32b78bef5a58
JustinMont07 <ja28mont@siena.edu>
2024-02-04 12:48:23 -0500

    Fixed the more info button and made it working for a few professors. Not updating others as we can wait until the Database is up.

--- SOSAR/Assets/Scripts/MoreInfo.cs
+++ SOSAR/Assets/Scripts/MoreInfo.cs
@@ -25,10 +25,19 @@ public class MoreInfo : MonoBehaviour
         
         //Get the elements of the UI
         Button backButton = root.Q<Button>("backButton");
-        VisualElement moreInfoText = root.Q<VisualElement>("InfoText");
+        Label moreInfoText = root.Q<Label>("InfoText");
+        Label professorNameText = root.Q<Label>("ProfName");
+        Label profDepartmentText = root.Q<Label>("ProfDepartment");
+        //Image profImage = root.Q<Image>("ProfImage");
 
         //When the back button is clicked load the roger bacon scene.
         backButton.clicked += () => SceneManager.LoadScene("RogerBacon");
+
+        //Update text based on info recieved from ModifyInfoButton script
+        moreInfoText.text = ModifyInfoButton.global_TextForNewScene;
+        professorNameText.text = ModifyInfoButton.global_ProfNameForNewScene;
+        profDepartmentText.text = ModifyInfoButton.global_ProfDepartmentForNewScene;
+        //profImage.image = ModifyInfoButton.global_ProfImageForNewScene;
     }
 
     // Update is called once per frame
