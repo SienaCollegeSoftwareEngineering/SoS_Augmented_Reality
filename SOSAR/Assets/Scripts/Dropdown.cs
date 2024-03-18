1279133785f465b9bc7161852e6e0a20c574670b
JustinMont07 <ja28mont@siena.edu>
2024-03-18 16:29:59 -0400

    Added some files that didn't go into original commit

---
+++ SOSAR/Assets/Scripts/Dropdown.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+
+public class Dropdown : MonoBehaviour
+
+
+{
+    int value;
+    public string selectedValue;
+    public void getSelectedValue(int val)
+    {
+        value = val;
+    }
+
+    public void changeScene(string whereFrom)
+    {
+
+        string[] professors = {"None", "RB308", "RB311", "RB313", "RB314", "RB316", "RB317", "RB318", "RB319", "RB320", "RB321", "RB323", "RB326", "RB332", "RB338"};
+
+        string[] classrooms = {"None", "RB302", "RB304", "RB306", "RB312", "RB322", "RB324", "RB328", "RB340", "RB348", "RB350"};
+
+        if(whereFrom == "Professors")
+            selectedValue = professors[value];
+        else
+            selectedValue = classrooms[value];
+
+        ModifyInfoButton.global_RoomNum = selectedValue;
+        if(value != 0){
+            Debug.Log("Going to " + selectedValue);
+            //Change scene to more info scene
+            SceneManager.LoadScene("More Info Scene");
+        }
+    }
+
+}
