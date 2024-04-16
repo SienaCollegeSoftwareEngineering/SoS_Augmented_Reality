4ab940998b9f177f0a7a276ac62e18b07a088c96
JustinMont07 <ja28mont@siena.edu>
2024-04-15 21:35:02 -0400

    Added Files

---
+++ SOSAR/Assets/Scripts/ProffesorMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ProffesorMenu : MonoBehaviour
+{
+    public string selectedValue;
+    //public static string global_whereFrom;
+    public void goToNewScene(string roomNum)
+    {
+
+        ClassroomMenu.global_whereFrom = "ProfessorLinks";
+
+        //string[] professors = {"None", "RB308", "RB311", "RB313", "RB314", "RB316", "RB317", "RB318", "RB319", "RB320", "RB321", "RB323", "RB326", "RB332", "RB338"};
+
+        //string[] classrooms = {"None", "RB302", "RB304", "RB306", "RB312", "RB322", "RB324", "RB328", "RB340", "RB348", "RB350"};
+
+        selectedValue = roomNum;
+
+        ModifyInfoButton.global_RoomNum = selectedValue;
+        
+        Debug.Log("Going to " + selectedValue);
+        //Change scene to more info scene
+        SceneManager.LoadScene("More Info Scene");
+        
+    }
+}
