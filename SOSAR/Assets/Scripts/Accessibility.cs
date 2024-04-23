dce2f43e9068da192eaba216a579c83f8e83872d
JustinMont07 <ja28mont@siena.edu>
2024-04-23 10:04:45 -0400

    Final Commit, added files

---
+++ SOSAR/Assets/Scripts/Accessibility.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+*   This sript is meant to be used for accessibility to always have it enabled at the start but turn off if the user clicks the button
+*   Not finished!
+*/
+public class Accessibility : MonoBehaviour
+{
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(ChangeScene.global_accessibilityEnabled)
+            UAP_AccessibilityManager.EnableAccessibility(true);	
+        else{
+             UAP_AccessibilityManager.EnableAccessibility(false);
+        }
+    }
+
+    public void disableAccessibility()
+    {
+        ChangeScene.global_accessibilityEnabled = false;
+        UAP_AccessibilityManager.EnableAccessibility(false);	
+    }
+    */
+
+}
